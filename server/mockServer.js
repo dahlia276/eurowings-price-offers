1adf2de79df82773f8e903ed92b3c26445f29868
Dahlia Elbanhawy <66922502+dahlia276@users.noreply.github.com>
2026-01-12 18:18:13 +0100

    feat: setup mock server & data

---
+++ server/mockServer.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import mockData from './mockData.json';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/price-offers', (req, res) => {
+  res.json(mockData);
+});
+
+app.listen(3001, () => {
+  console.log('Mock server running on port 3001');
+});
