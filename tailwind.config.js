f92001d1de3cf6457d49063101ebf5a945f50365
dahlia.elbanhawy <dahlia.elbanhawy@real-digital.de>
2026-01-13 01:14:01 +0100

    chore: update dependencies and add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Make sure this includes your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
